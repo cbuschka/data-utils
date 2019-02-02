26e5f2c70c29c16efd0251d9130158356c271e0f
conni <c.buschka@arcusx.com>
2019-02-02 11:19:37 +0100

    Improved split path.

--- src/paths/__tests__/split_test.js
+++ src/paths/__tests__/split_test.js
@@ -7,3 +7,27 @@ it('split returns single part', () => {
 it('split returns all parts', () => {
     expect(split('a.b.c')).toEqual(['a', 'b', 'c']);
 });
+
+it('split returns array parts', () => {
+    expect(split('a.b[1]')).toEqual(['a', 'b', 1]);
+});
+
+it('split returns array embedded parts', () => {
+    expect(split('a.b[1][2].c')).toEqual(['a', 'b', 1, 2, 'c']);
+});
+
+it('split detects double dot', () => {
+    expect(() => split('a..c')).toThrow(Error);
+});
+
+it('split detects empty array index', () => {
+    expect(() => split('a[]')).toThrow(Error);
+});
+
+it('split detects dangling array index', () => {
+    expect(() => split('a[1')).toThrow(Error);
+});
+
+it('split detects missing dot', () => {
+    expect(() => split('a[1]b')).toThrow(Error);
+});
--- src/paths/split.js
+++ src/paths/split.js
@@ -1,3 +1,69 @@
+const EOF = {};
+
 export const split = (string) => {
-    return string.split(/\./);
+
+    const path = [];
+    let prevPos = 0;
+    let state = 0; // 0=in text, 1=in array index, 2 = after array index
+    for (let pos = 0; pos < string.length + 1; ++pos) {
+        const c = string[pos] || EOF;
+        switch (state) {
+            case 0:
+                if (c === '.') {
+                    if (prevPos === pos) {
+                        throw new Error("Double dot at " + pos + ".");
+                    }
+                    path.push(string.substring(prevPos, pos));
+                    prevPos = pos + 1;
+                } else if (c === '[') {
+                    path.push(string.substring(prevPos, pos));
+                    prevPos = pos + 1;
+                    state = 1;
+                } else if (c === EOF) {
+                    if (prevPos < pos) {
+                        path.push(string.substring(prevPos, pos));
+                    }
+                } else {
+                    // ignored
+                }
+                break;
+            case 1:
+                if (c === ']') {
+                    if (prevPos === pos) {
+                        throw new Error("Empty array index at " + pos + ".");
+                    }
+                    path.push(parseInt(string.substring(prevPos, pos)));
+                    prevPos = pos + 1;
+                    state = 2;
+                } else if (c === EOF) {
+                    throw new Error("Dangling array index at " + pos + ".");
+                } else {
+                    // ok
+                }
+                break;
+            case 2:
+                if (c === '.') {
+                    prevPos = pos + 1;
+                    state = 0;
+                } else if (c === '[') {
+                    prevPos = pos + 1;
+                    state = 1;
+                } else if (c === EOF) {
+                    state = 0;
+                    //  ok
+                } else {
+                    throw new Error(". or [ expected at " + pos + ".");
+                }
+                break;
+            default :
+                throw new Error("Unknown state " + state + " while at pos " + pos + ".");
+        }
+    }
+
+    if (state !== 0) {
+        throw new Error();
+    }
+
+
+    return path;
 };
